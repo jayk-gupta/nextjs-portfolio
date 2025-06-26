88b561d24c80c05dafd8acf301981669989a38fd
jay kumar gupta <jayesh.gupta3224@gmail.com>
2025-06-26 14:25:17 +0530

    created home page strcuture and sections

---
+++ src/app/components/HomePage/FeaturedProjects.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import projects from "@/data/projects.json";
+import Link from "next/link";
+
+const featured = projects.filter((p) => p.featured);
+function FeaturedProjects() {
+  return (
+    <div>
+      <h2>Featured Projects</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        {featured.map((project) => (
+          <Link key={project.slug} href={`/projects/${project.slug}`}>
+            <div className="border p-4 rounded hover:shadow-md">
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+      <Link href="/projects" className="text-blue-600 mt-4 inline-block">
+        View All Projects →
+      </Link>
+    </div>
+  );
+}
+
+export default FeaturedProjects;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,17 @@
-import projects from "../../data/projects.json";
-import Link from "next/link";
-
-const featured = projects.filter((p) => p.featured);
+import About from "./components/HomePage/About";
+import Contact from "./components/HomePage/Contact";
+import Hero from "./components/HomePage/Hero";
+import Skills from "./components/HomePage/Skills";
+import ProjectsList from "./projects/page";
 
 export default function Home() {
   return (
-    <section>
-      <h2>Featured Projects</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        {featured.map((project) => (
-          <Link key={project.slug} href={`/projects/${project.slug}`}>
-            <div className="border p-4 rounded hover:shadow-md">
-              <h3>{project.title}</h3>
-              <p>{project.description}</p>
-            </div>
-          </Link>
-        ))}
-      </div>
-      <Link href="/projects" className="text-blue-600 mt-4 inline-block">
-        View All Projects →
-      </Link>
-    </section>
+    <div>
+      <Hero />
+      <About />
+      <Skills />
+      <ProjectsList />
+      <Contact />
+    </div>
   );
 }
